19e34e34bf17f1be2e064b3b4432c6b10e3e0e7c
lukaulu <jonathan.mambuma@elorrieta-errekamari.com>
2026-01-16 12:48:42 +0100

    Proyecto maui subido

---
+++ ElorMAUI/Models/Centro.cs
@@ -0,0 +1,20 @@
+﻿namespace ElorMAUI.Models
+{
+    public class RootCentros
+    {
+        public List<Centro> CENTROS { get; set; }
+    }
+
+    public class Centro
+    {
+        public int CCEN { get; set; }
+        public string NOM { get; set; }
+        public string DMUNIC { get; set; }
+        public string DTERRE { get; set; }
+        public string DTITUC { get; set; }
+        public string DOMI { get; set; }
+        public string EMAIL { get; set; }
+        public double LATITUD { get; set; }
+        public double LONGITUD { get; set; }
+    }
+}
---
+++ ElorMAUI/Services/CentroService.cs
@@ -0,0 +1,33 @@
+﻿using ElorMAUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ElorMAUI.Services
+{
+    public class CentroService
+    {
+        public async Task<List<Centro>> GetCentros()
+        {
+            // Abrir el JSON desde el Resources
+            using var stream = await FileSystem.OpenAppPackageFileAsync("EuskadiLatLon.json");
+            using var reader = new StreamReader(stream);
+            var contents = await reader.ReadToEndAsync();
+
+            // Ignoro mayúsculas y minúsculas en los nombres de las propiedades
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            // Convertir el texto JSON a la clase RootCentros
+            var data = JsonSerializer.Deserialize<RootCentros>(contents, options);
+
+            // Deuelvo la lista de centros, si es nulo devuelvo una lista vacía para evitar errores
+            return data?.CENTROS ?? new List<Centro>();
+        }
+    }
+}
